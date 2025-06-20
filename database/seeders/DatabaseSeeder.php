b8254a8d1ce4104effecd2243f4f2c301d23983f
Chun Choeurn <151984005+ChoeurnDesign@users.noreply.github.com>
2025-06-20 12:24:24 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class, // Ensure categories are seeded first
             BookSeeder::class,     // Then seed your books and attach categories
-            UserSeederr::class,
+            // UserSeederr::class,
             // Add other seeders here as you create them, e.g., ReviewSeeder::class, etc.
         ]);
     }
