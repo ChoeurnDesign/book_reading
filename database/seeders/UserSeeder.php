13fed86a4152c1da0c1ed22f40521338a483b92b
Chun Choeurn <151984005+ChoeurnDesign@users.noreply.github.com>
2025-06-20 11:30:26 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,32 +1,28 @@
-
 <?php
 
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User; // Make sure the User model namespace is correct
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         // Create Admin user
         User::create([
             'name' => 'Choeurn',
-            'email' => 'chunchoeurn99.com',
-            'password' => Hash::make('Choeurn123'), // Change to a secure password
+            'email' => 'chunchoeurn99@gmail.com',
+            'password' => Hash::make('Choeurn123'),
             'role' => 'admin', // Only if your users table has a 'role' column
         ]);
 
         // Create normal User
         User::create([
             'name' => 'User',
-            'email' => 'user123@gmail.com',
-            'password' => Hash::make('User123'), // Change to a secure password
+            'email' => 'user@example.com',
+            'password' => Hash::make('User12345'),
             'role' => 'user', // Only if your users table has a 'role' column
         ]);
     }
